53cf376a94983ee1334cee355be3fa8d29adb3b0
Mehdi Rajabi <166663403+M-Rajabi-dev@users.noreply.github.com>
2025-12-16 21:29:17 +0330

    t16

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -2,12 +2,12 @@ ExternalProject_Add(mpv
     DEPENDS
         ffmpeg
         libiconv
-        rubberband
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
+        --wipe
         --prefix=${MINGW_INSTALL_PREFIX}
         --libdir=${MINGW_INSTALL_PREFIX}/lib
         --cross-file=${MESON_CROSS}
@@ -28,7 +28,7 @@ ExternalProject_Add(mpv
         -Dlibbluray=disabled
         -Ddvdnav=disabled
         -Duchardet=disabled
-        -Drubberband=enabled
+        -Drubberband=disabled
         -Dlcms2=disabled
         -Dopenal=disabled
         -Dspirv-cross=disabled
@@ -50,14 +50,6 @@ ExternalProject_Add(mpv
     BUILD_COMMAND ${EXEC} LTO_JOB=1 PDB=1 ninja -C <BINARY_DIR>
     INSTALL_COMMAND ""
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-)
-
-ExternalProject_Add_Step(mpv nuke-existing-build
-    DEPENDEES update
-    DEPENDERS configure
-    COMMAND ${CMAKE_COMMAND} -E remove_directory <BINARY_DIR>
-    COMMAND ${CMAKE_COMMAND} -E make_directory <BINARY_DIR>
-    ALWAYS 1
 )
 
 ExternalProject_Add_Step(mpv strip-binary
